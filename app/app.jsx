8ff7728110d24020b51dc701efe0547a3314ec57
Paul Zuege <paul@zuege-web.de>
2016-10-01 17:18:25 +0200

    Add foundation & jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -6,10 +6,13 @@ var Nav = require('Nav');
 var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
+
+//Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 //Syntax:
 //var Route = require('react-router').Route;
-
-
 
 ReactDOM.render(
   <Router history={hashHistory}>
--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,11 @@ var About = (props) => {
   return (
     <div>
       <h3>About</h3>
-      This is a neat little React Project, that can actually show you the current temperatur
-      at any city you like!
+      <p>
+        This is a neat little React Project, that can actually show you the current temperatur
+        at any city you like!
+      </p>
+      <p>By the way, hallo Julia :)</p>
     </div>
   );
 };
