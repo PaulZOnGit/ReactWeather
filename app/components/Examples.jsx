3bd09fc0bb1b268cd3bd97daec7cdef85b9914b5
Paul Zuege <paul@zuege-web.de>
2016-10-01 16:00:51 +0200

    Rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -5,7 +5,7 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples Component!</h3>
+      <h3>Examples</h3>
       <p>Welcome to examples page!</p>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
       <br/>
