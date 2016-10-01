07c244a6a468180e8fdfded5810fc8742e28b2f6
Paul Zuege <paul@zuege-web.de>
2016-10-01 16:09:30 +0200

    Rename about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,17 +1,9 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>About Component</h3>
-//     );
-//   }
-// });
-
 var About = (props) => {
   return (
     <div>
-      <h3>About Component</h3>
+      <h3>About</h3>
       This is a neat little React Project, that can actually show you the current temperatur
       at any city you like!
     </div>
